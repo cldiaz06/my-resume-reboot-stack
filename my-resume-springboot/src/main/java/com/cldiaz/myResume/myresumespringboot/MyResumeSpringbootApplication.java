8d3bfbbfaac6d786114ee919408cd3f194d37148
cldiaz06 <cldiaz06@gmail.com>
2019-12-30 21:13:56 -0600

    Initialize reboot stack

---
+++ my-resume-springboot/src/main/java/com/cldiaz/myResume/myresumespringboot/MyResumeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cldiaz.myResume.myresumespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResumeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResumeSpringbootApplication.class, args);
+	}
+
+}
---
+++ my-resume-springboot/src/test/java/com/cldiaz/myResume/myresumespringboot/MyResumeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cldiaz.myResume.myresumespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResumeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
